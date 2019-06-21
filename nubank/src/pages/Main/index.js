7090aeef1401d6e9422f5dbb13ffef0085f40018
sergiomachadopk <sergio.machadopk@gmail.com>
2019-06-21 16:42:38 -0300

    First Commit

---
+++ nubank/src/pages/Main/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container } from './styles'
+
+import Header from '~/components/Header'
+
+export default function Main(){
+
+  return( 
+    <Container />
+  )
+}
