ad37ab1c10576d953dd3ee2ba096da84b91edc17
sergiomachadopk <sergio.machadopk@gmail.com>
2019-06-28 12:58:07 -0300

    Touchable Adicionado

--- nubank/src/components/Tabs/styles.js
+++ nubank/src/components/Tabs/styles.js
@@ -14,7 +14,7 @@ export const TabsContainer = styled.ScrollView.attrs({
 
 })``;
 
-export const TabItem = styled.View `
+export const TabItem = styled.TouchableOpacity `
     width: 100px;
     height: 100px;
     background: rgba(255,255,255,0.2);
